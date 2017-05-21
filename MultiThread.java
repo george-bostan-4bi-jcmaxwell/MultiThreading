29ea293d4a62af0f7be369b41f42adb614cf9cc3
george-bostan-4bi-jcmaxwell <george.bostan@jcmaxwell.it>
2017-05-21 11:04:41 +0200

    Update MultiThread.java

--- MultiThread.java
+++ MultiThread.java
@@ -13,9 +13,7 @@
 //ripeto quindi che il codice è stato fatto da me tranne per i due if finali che ho fatto con l'aiuto di gerardo.
 package multithread;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import static multithread.TicTacToe.punteggio;
 /**
  *
  * @author George Bostan
@@ -33,13 +31,13 @@ public class MultiThread {
     // viene infine mostrato il punteggio che rappresenta le volte dove il thread TOE capita prima di TAC
     public static void main(String[] args) {
         System.out.println("Main Thread iniziata...");
-        long start = System.currentTimeMillis();
+        Schermi x = new Schermi();
         
-        Thread tic = new Thread (new TicTacToe("TIC")); //permette la creazione primo THREAD TIC
+        Thread tic = new Thread (new TicTacToe("TIC", x)); //permette la creazione primo THREAD TIC
         
-        Thread tac = new Thread(new TicTacToe("TAC"));  //permette la creazione secondo THREAD TAC
+        Thread tac = new Thread(new TicTacToe("TAC", x));  //permette la creazione secondo THREAD TAC
         
-        Thread toe = new Thread(new TicTacToe("TOE"));  //permette la creazione terzo THREAD TOE
+        Thread toe = new Thread(new TicTacToe("TOE", x));  //permette la creazione terzo THREAD TOE
         
         tic.start();    // avvia il THREAD TIC
         tac.start();    // avvia il THREAD TAC
@@ -60,58 +58,54 @@ public class MultiThread {
             TimeUnit.MILLISECONDS.sleep(1111);  //tempo di attesa
         } catch (InterruptedException e) {}
 
-        long end = System.currentTimeMillis();
-        System.out.println("Il punteggio e: " +punteggio);  //mostra su schermo il punteggio finale calcolato nel run()
+        System.out.println("Il punteggio e: " +x.punteggio());  //mostra su schermo il punteggio finale calcolato nel run()
         System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms"); //indica il tempo impiegato dal programma
     }
     
 }
 
 class TicTacToe implements Runnable {
-    public static int punteggio = 0;    //variabile utilizzata calcolare e stampare il punteggio finale
-    public static boolean trovato = false;  //variabile utilizzata durante il calcolo del punteggio
     private String t;
     private String msg;
+    Schermi x;
 
     // Costruttore
-    public TicTacToe (String s) {
+    public TicTacToe (String s, Schermi x) {
         this.t = s;
+        this.x = x;
     }
-    
+    class Schermi {
+
+  String ultimoTHREAD = ""; // ultimo thread che ha scritto sullo schermo
+  int punteggio = 0;
+
+  public int punteggio() {  // fornisce il punteggio
+    return this.punteggio;
+  }
+
+  public synchronized void scrivi(String thread, String msg) {
+    int casuale=100+(int)(Math.random()*300); //numero casuale tra 100 e 400
+    msg += ": " + casuale + " :";
+    if( thread.equals("TOE") && ultimoTHREAD.equals("TAC")) {
+        punteggio++;
+        msg += "  <----------------";
+    }
+    try {
+        TimeUnit.MILLISECONDS.sleep(casuale); //casuale ora diventa un numero rappresentante il tempo il MILLISECONDI
+    } catch (InterruptedException e) {} //Richiamo eccezione    this.ultimoTHREAD = thread;
+    System.out.println(msg);
+    ultimoTHREAD = thread;
+  }
+}
     @Override // Annotazione per il compilatore
     // se facessimo un overloading invece di un override il copilatore ci segnalerebbe l'errore
     // per approfondimenti http://lancill.blogspot.it/2012/11/annotations-override.html
     // questa è la parte del codice che permette di calcolare sia il tempo random, sia il punteggio finale
     // esso utilizza poi il tempo creato per avviare i THREADS in modo casuale
     public void run() {
-        Random random = new Random();   //serve per creare i valori random
-        int j = 100;
-        int n = 300-j;
-        int k = random.nextInt(n)+j;    //i valori saranno compresi tra 100 e 300
         for (int i = 10; i > 0; i--) {
-            if (t.equals("TAC"))    //controlla se è in corso il THREAD TAC
-            {
-                trovato = true; //se la condizione è vera trovato diventa true
-            }
-            msg = "<" + t + "> ";
-            //System.out.print(msg);
-            try {
-                TimeUnit.MILLISECONDS.sleep(k);
-            } catch (InterruptedException e) {
-                System.out.println("THREAD " + t + " e' stata interrotta! bye bye...");
-                return; //me ne vado = termino il THREAD
-            }
-            if ("TOE".equals(t) && trovato == true) //controlla se il THREAD TOE è in corso e se la prima condizione è = true
-            {
-                punteggio++;    //se la condizione si avvera il punteggio viene incrementato
-            }
-            else
-            {
-                trovato = false;    //altrimenti trovato diventa false e di conseguenza il punteggio rimane invariato
-            }
-            msg += t + ": " + i;
-            System.out.println(msg);
-         
+            msg = "<" + t + "> " + t + ": " + i;
+            x.scrivi(t, msg);
         }
     }
     
